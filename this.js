bc37b19228de0048412caf67ef342ca183b95038
pathak-arpita <pathak.arpita6497@gmail.com>
2025-06-24 13:12:37 +0530

    this keyword - 2

--- this.js
+++ this.js
@@ -14,7 +14,7 @@ console.log(this);
 //! Inside the function ---------------------------->
 function x() {
     // value of this keyword will be different based on strick mode or non-strick mode.
-    console.log("this inside x function ----->",this);
+    console.log("this inside x function ----->", this);
 }
 
 x();
@@ -31,10 +31,86 @@ x();
 //! "This keyword" value is depends on how the function is called ---------------------------->
 
 function y() {
-    console.log("this inside y function ----->",this);
+    console.log("this inside y function ----->", this);
 }
 
 y(); // OUTPUT --------> window (Non-strict mode) or undefined (Strict mode)
 window.y(); // OUTPUT --------> window
 
 //---------------------------------------------------------------------------------------------//
+
+//! Inside the Object ---------------------------->
+
+const obj = {
+    a: 10,
+    // x is the methid of the object
+    x: function () {
+        console.log("this inside x function of obj ----->", this);
+        console.log("value of a ----->", this.a);
+    }
+}
+
+obj.x(); // OUTPUT --------> obj
+
+//---------------------------------------------------------------------------------------------//
+
+//! Call , Apply and Bind ----------------------------> by using them we can update the value of "this keyword".
+
+const student_1 = {
+    name: "arpita",
+    printName: function () {
+        console.log("this inside printName function ----->", this.name);
+    }
+}
+
+const student_2 = {
+    name: "suman",
+}
+
+// Using call method
+student_1.printName.call(student_2); // OUTPUT --------> suman
+
+// Using apply method
+student_1.printName.apply(student_2); // OUTPUT --------> suman     
+
+// Using bind method
+const printName_2 = student_1.printName.bind(student_2);
+printName_2(); // OUTPUT --------> suman
+
+//---------------------------------------------------------------------------------------------//
+
+//! Arrow Fuctions ---------------------------->  Arrow functions do not have their own "this" context, they inherit "this" from the parent scope.
+
+const obj_arrowFuction = {
+    x: 20,
+    y: () => {
+        console.log("this inside y function of obj_arrowFuction ----->", this);
+    }
+}
+
+obj_arrowFuction.y(); // OUTPUT --------> window
+
+// Nested Arrow Function ------>
+//^ 1.
+const obj_nestedArrowFunction = {
+    x: 30,
+    y: function () {
+        const z = () => {
+            console.log("this inside nested arrow function ----->", this);
+        }
+        z();
+    }
+}
+obj_nestedArrowFunction.y(); // OUTPUT --------> obj_nestedArrowFunction
+
+//^ 2.
+const obj_nestedArrowFunction2 = {
+    x: 30,
+    y: () => {
+        const z = () => {
+            console.log("this inside nested arrow function - example 2 ----->", this);
+        }
+        z();
+    }
+}
+obj_nestedArrowFunction2.y(); // OUTPUT --------> window
