79ea47223ab2378b9aa49f6e5e8d913b0b1ea518
pathak-arpita <pathak.arpita6497@gmail.com>
2025-06-26 18:06:31 +0530

    adding events in js

--- Events & EventLoop/Events.js
+++ Events & EventLoop/Events.js
@@ -6,31 +6,37 @@
 //^ Event capturing : 
 // Event bubbling means the event starts from the clicked element (target) and goes up to its parent elements, one by one.
 
-//~NOTE -----------------------> 
+//~ NOTE -----------------------> 
 //*          1). the default nature of events in JavaScript is bubbling.
 //*          2). To make it a capturing event, we have to pass " TRUE " as the third argument in addEventListener.
 
+//~ NOTE -----------------------> 
+//* " e.stopPropagation() " is a method used inside an event handler to stop the event from bubbling or capturing further.
+
+
 
 document.querySelector("#greatGrandParents")
-.addEventListener("click" , ()=>{
-    console.log("Great Grand Parent div is clicked")
-},true)
+    .addEventListener("click", () => {
+        console.log("Great Grand Parent div is clicked")
+    }, true)     //* Event capturing 
 
 
 document.querySelector("#grandParents")
-.addEventListener("click" , ()=>{
-    console.log("Grand Parent div is clicked")
-},false)
+    .addEventListener("click", () => {
+        console.log("Grand Parent div is clicked")
+    }, false)     //^ Event Bubbling
 
 document.getElementById("parents")
-.addEventListener("click" , ()=>{
-    console.log("Parent div is clicked")
-},true)
+    .addEventListener("click", () => {
+       
+        console.log("Parent div is clicked")
+    }, true)     //* Event capturing
 
 document.getElementById("child")
-.addEventListener("click" , ()=>{
-    console.log("Child div is clicked")
-},false)
+    .addEventListener("click", (e) => {
+        console.log("Child div is clicked")
+         e.stopPropagation();
+    }, false)     //^ Event Bubbling
 
 // OUTPUT ----------------------> WHEN CLICK ON CHILD DIV
 //     1 .  Great Grand Parent div is clicked
@@ -43,3 +49,9 @@ document.getElementById("child")
 //     1 .  Great Grand Parent div is clicked
 //     2 .  Parent div is clicked
 //     3 .  Grand Parent div is clicked
+
+
+// OUTPUT ----------------------> WHEN e.stopPropagation();
+//     1 .  Great Grand Parent div is clicked
+//     2 .  Parent div is clicked
+//     3 .  Child div is clicked
