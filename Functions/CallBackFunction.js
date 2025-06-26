40789fa68e6cfb883c92098e62adfc1e6e9a8dc9
pathak-arpita <pathak.arpita6497@gmail.com>
2025-06-26 15:28:18 +0530

    adding HOF

--- Functions/CallBackFunction.js
+++ Functions/CallBackFunction.js
@@ -1,5 +1,5 @@
-//!A callback function is a function that is passed as an argument to another function 
-//! and is called later after some work is done.
+//!    A callback function is a function that is passed as an argument to another function 
+//!    and is called later after some work is done.
 
 
 // Example 1).
---
+++ Functions/HigherOrderFunction.js
@@ -0,0 +1,26 @@
+//!  A function takes " another function " as argument or " rturns a function " is call "Higher order Function" .
+
+
+//^  Example 1: ------------> Passing a function as an argument
+
+function greet(name) {
+    return `Hi!! My name is ${name}.`
+}
+
+function higherOrderFunc(callback) {
+    console.log("its higher order function -----> ", callback("Arpita"));
+}
+
+higherOrderFunc(greet);    //----------------------> higherOrderFunc is Higher order Function. 
+
+
+//^  Example 1: ------------> Returning Function  
+
+function multiply(x) {
+    return function (y) {
+        console.log("returning a function ----> ", y * x);
+    }
+}
+
+const ans = multiply(2);
+ans(23);
